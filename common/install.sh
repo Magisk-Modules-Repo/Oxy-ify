006975af00350aa67f2ae95c08eba1843cd6c375
Filippo Fedeli <43090263+MarcAnt01@users.noreply.github.com>
2019-03-07 17:16:14 +0100

    Added installation options per apps

--- common/install.sh
+++ common/install.sh
@@ -43,22 +43,89 @@ else
   rm -rf "$INSTALLER"/system/media/audio
 fi
 
-if [ "$API" -ge 24 ]; then
+if [ "$API" -ge 27 ]; then
   ui_print " "
-  ui_print " - App Option -"
   ui_print " *******************************************"
-  ui_print "   Do you want OnePlus apps?"
-  ui_print "   Included apps are Camera, Gallery, Sound Recorder,"
-  ui_print "   Widget and Weather"
+  ui_print "   Do you want OnePlus Gallery?"
   ui_print "   Vol Up = Yes, Vol Down = No"
   if $VKSEL; then
     ui_print " "
-    ui_print "   Enabling OnePlus apps..."
+    ui_print "   Enabling OnePlus Gallery..."
   else
     ui_print " "
-    ui_print "   Disabling OnePlus apps..."
-    rm -rf "$INSTALLER"/system/priv-app/
+    ui_print "   Disabling OnePlus Gallery..."
+    rm -rf "$INSTALLER"/system/priv-app/OPGallery
   fi
 else
-  rm -rf "$INSTALLER"/system/priv-app/
+  rm -rf "$INSTALLER"/system/priv-app/OPGallery
+fi
+
+if [ "$API" -ge 24 ]; then
+  ui_print " "
+  ui_print " *******************************************"
+  ui_print "   Do you want OnePlus Camera?"
+  ui_print "   Vol Up = Yes, Vol Down = No"
+  if $VKSEL; then
+    ui_print " "
+    ui_print "   Enabling OnePlus Camera..."
+  else
+    ui_print " "
+    ui_print "   Disabling OnePlus Camera..."
+    rm -rf "$INSTALLER"/system/priv-app/OPCamera
+  fi
+else
+  rm -rf "$INSTALLER"/system/priv-app/OPCamera
+fi
+
+if [ "$API" -ge 23 ]; then
+  ui_print " "
+  ui_print " *******************************************"
+  ui_print "   Do you want OnePlus Sound Recorder?"
+  ui_print "   Vol Up = Yes, Vol Down = No"
+  if $VKSEL; then
+    ui_print " "
+    ui_print "   Enabling OnePlus Sound Recorder..."
+  else
+    ui_print " "
+    ui_print "   Disabling OnePlus Sound Recorder..."
+    rm -rf "$INSTALLER"/system/priv-app/OPRecorder
+  fi
+else
+  rm -rf "$INSTALLER"/system/priv-app/OPRecorder
+fi
+
+if [ "$API" -ge 25 ]; then
+  ui_print " "
+  ui_print " *******************************************"
+  ui_print "   Do you want OnePlus Weather and Widget?"
+  ui_print "   Vol Up = Yes, Vol Down = No"
+  if $VKSEL; then
+    ui_print " "
+    ui_print "   Enabling OnePlus Weather..."
+    ui_print "   Enabling OnePlus Widget..."
+  else
+    ui_print " "
+    ui_print "   Disabling OnePlus Weather..."
+    rm -rf "$INSTALLER"/system/priv-app/OPWeather
+    rm -rf "$INSTALLER"/system/priv-app/OPWidget
+  fi
+else
+  rm -rf "$INSTALLER"/system/priv-app/OPWeather
+  rm -rf "$INSTALLER"/system/priv-app/OPWidget
+
+if [ "$API" -ge 27 ]; then
+  ui_print " "
+  ui_print " *******************************************"
+  ui_print "   Do you want OnePlus Launcher?"
+  ui_print "   Vol Up = Yes, Vol Down = No"
+  if $VKSEL; then
+    ui_print " "
+    ui_print "   Enabling OnePlus Launcher..."
+  else
+    ui_print " "
+    ui_print "   Disabling OnePlus Launcher..."
+    rm -rf "$INSTALLER"/system/priv-app/OPLauncher
+  fi
+else
+  rm -rf "$INSTALLER"/system/priv-app/OPLauncher
 fi
