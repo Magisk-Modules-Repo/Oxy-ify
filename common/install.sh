95e5648a2b70a7d6bbe3315a8e73395d060f6f47
Filippo <fedeli.filippo@gmail.com>
2020-05-06 12:03:18 +0200

    Hide Screen Recorder for Android 10 It can't be installed, so directly removed the option

--- common/install.sh
+++ common/install.sh
@@ -39,7 +39,7 @@ if [ $API -ge 27 ]; then
   fi
 fi
 
-if [ $API -ge 24 ]; then
+if [ $API -ge 24 || $API -le 29 ]; then
   ui_print " "
   ui_print " *******************************************"
   ui_print "   Do you want OnePlus Screen Recorder?"
